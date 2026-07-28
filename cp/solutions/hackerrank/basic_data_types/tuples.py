dd10bb36a11f5b18441b767d6c00aff22ce768e1
Andrew Virya Victorio <avv210@Andrews-MacBook-Pro.local>
2026-07-28 08:53:21 +0700

    update solutions

--- cp/solutions/hackerrank/basic_data_types/tuples.py
+++ cp/solutions/hackerrank/basic_data_types/tuples.py
@@ -4,5 +4,7 @@ if __name__ == '__main__':
 
     # Solution not accepted because the hash() functions algorithm different in Pypy3 and Python 3.
     # Both runtime are not accepted.
+
+    # Update: use Python 2 runtim
     t = tuple(integer_list)
     print(hash(t))
